5769a48f098c55ffbd9a94a8978574a915e68852
Vita <vitalia@myrambler.ru>
2019-09-15 14:21:22 +0300

    + exception handling

--- KursovayaBD/Form1.cs
+++ KursovayaBD/Form1.cs
@@ -61,35 +61,43 @@ namespace KursovayaBD
 
         private void mbutton4_Click(object sender, EventArgs e)//filter button
         {
-            if (textBox1.Text == "" || comboBox1.Text=="")
+            try
             {
-                MessageBox.Show("One of boxes was left empty, try again/"); 
+                if (textBox1.Text == "" || comboBox1.Text == "")
+                {
+                    MessageBox.Show("One of boxes was left empty, try again.");
+                }
+                else
+                {
+                    string sql = "";
+                    string connectionString = @"Data Source=DESKTOP-72MPP4U\SQLEXPRESS;Initial Catalog=usersdb;Integrated Security=True";
+                    if (comboBox1.Text == "Ticket_class")
+                    {
+                        sql = "SELECT * FROM Ticket WHERE Ticket_class ='" + textBox1.Text + "'";
+                    }
+                    if (comboBox1.Text == "Passenger_surname")
+                    {
+                        sql = "SELECT * FROM Ticket WHERE Passenger_surname ='" + textBox1.Text + "'";
+                    }
+                    if (comboBox1.Text == "Ticket_id")
+                    {
+                        sql = "SELECT * FROM Ticket WHERE Ticket_id ='" + textBox1.Text + "'";
+                    }
+                    Form1 f = new Form1();
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+                        DataSet ds = new DataSet();
+                        adapter.Fill(ds);
+                        dataGridView1.DataSource = ds.Tables[0];
+                    }
+                }
             }
-            else
+            catch (SqlException ex)
             {
-                string sql = "";
-                string connectionString = @"Data Source=DESKTOP-72MPP4U\SQLEXPRESS;Initial Catalog=usersdb;Integrated Security=True";
-                if (comboBox1.Text == "Ticket_class")
-                {
-                     sql = "SELECT * FROM Ticket WHERE Ticket_class ='"+textBox1.Text+"'";
-                }
-                if (comboBox1.Text == "Passenger_surname")
-                {
-                     sql = "SELECT * FROM Ticket WHERE Passenger_surname ='" + textBox1.Text + "'";
-                }
-                if (comboBox1.Text == "Ticket_id")
-                {
-                    sql = "SELECT * FROM Ticket WHERE Ticket_id ='" + textBox1.Text + "'";
-                }
-                Form1 f = new Form1();
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
-                    DataSet ds = new DataSet();
-                    adapter.Fill(ds);
-                    dataGridView1.DataSource = ds.Tables[0];
-                }
+                MessageBox.Show($"Exception occured. \n {ex}.\n Please return and retry.");
+                return;
             }
         }
 
@@ -129,26 +137,34 @@ namespace KursovayaBD
 
         private void mbutton5_Click(object sender, EventArgs e)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            try
             {
-                connection.Open();
-                adapter = new SqlDataAdapter(sql, connection);
-                commandBuilder = new SqlCommandBuilder(adapter);
-                adapter.InsertCommand = new SqlCommand("sp_CreateTicket", connection);
-                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Ticket_id", SqlDbType.Int, 50, "Ticket_id"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Ticket_class", SqlDbType.VarChar, 50, "Ticket_class"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Place", SqlDbType.SmallInt, 50, "Place"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Passenger_surname", SqlDbType.VarChar, 50, "Passenger_surname"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Passenger_name", SqlDbType.VarChar, 50, "Passenger_name"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Passenger_middlename", SqlDbType.VarChar, 50, "Passenger_middlename"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Price", SqlDbType.Money, 10, "Price"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Discount", SqlDbType.Bit, 0, "Discount"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Direction_id", SqlDbType.Int, 50, "Direction_id"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Flight_id", SqlDbType.Int, 50, "Flight_id"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Aircraft_id", SqlDbType.Int, 50, "Aircraft_id"));
-                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Pilot_id", SqlDbType.Int, 50, "Pilot_id"));
-                adapter.Update(ds);
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    adapter = new SqlDataAdapter(sql, connection);
+                    commandBuilder = new SqlCommandBuilder(adapter);
+                    adapter.InsertCommand = new SqlCommand("sp_CreateTicket", connection);
+                    adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+                    adapter.InsertCommand.Parameters.Add(new SqlParameter("@Ticket_id", SqlDbType.Int, 50, "Ticket_id"));
+                    adapter.InsertCommand.Parameters.Add(new SqlParameter("@Ticket_class", SqlDbType.VarChar, 50, "Ticket_class"));
+                    adapter.InsertCommand.Parameters.Add(new SqlParameter("@Place", SqlDbType.SmallInt, 50, "Place"));
+                    adapter.InsertCommand.Parameters.Add(new SqlParameter("@Passenger_surname", SqlDbType.VarChar, 50, "Passenger_surname"));
+                    adapter.InsertCommand.Parameters.Add(new SqlParameter("@Passenger_name", SqlDbType.VarChar, 50, "Passenger_name"));
+                    adapter.InsertCommand.Parameters.Add(new SqlParameter("@Passenger_middlename", SqlDbType.VarChar, 50, "Passenger_middlename"));
+                    adapter.InsertCommand.Parameters.Add(new SqlParameter("@Price", SqlDbType.Money, 10, "Price"));
+                    adapter.InsertCommand.Parameters.Add(new SqlParameter("@Discount", SqlDbType.Bit, 0, "Discount"));
+                    adapter.InsertCommand.Parameters.Add(new SqlParameter("@Direction_id", SqlDbType.Int, 50, "Direction_id"));
+                    adapter.InsertCommand.Parameters.Add(new SqlParameter("@Flight_id", SqlDbType.Int, 50, "Flight_id"));
+                    adapter.InsertCommand.Parameters.Add(new SqlParameter("@Aircraft_id", SqlDbType.Int, 50, "Aircraft_id"));
+                    adapter.InsertCommand.Parameters.Add(new SqlParameter("@Pilot_id", SqlDbType.Int, 50, "Pilot_id"));
+                    adapter.Update(ds);
+                }
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show($"Exception occured. \n {ex}.\n Please return and retry.");
+                return;
             }
         }
 
