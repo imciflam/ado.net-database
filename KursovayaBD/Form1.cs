edba63cbc4e3b9d97ee7940a385450645fdc9259
Vita <vitalia@myrambler.ru>
2019-09-28 12:44:12 +0300

    + grid row fix

--- KursovayaBD/Form1.cs
+++ KursovayaBD/Form1.cs
@@ -305,7 +305,7 @@ namespace KursovayaBD
                 worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
             }
             // storing Each row and column value to excel sheet  
-            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
             {
                 for (int j = 0; j < dataGridView1.Columns.Count; j++)
                 {
@@ -315,6 +315,7 @@ namespace KursovayaBD
             // save the application  
             workbook.SaveAs("C:\\Users\\Vitalia\\Desktop\\reports\\output.xlsx", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
             // Exit from the application  
+            MessageBox.Show("Data Exported");
             app.Quit();
             }
             catch(Exception ex)
